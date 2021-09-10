7e100e41fc1766e38a3c373a180eb42854ed05db
Maroomir Yoon <maroomir@gmail.com>
2021-09-11 00:59:59 +0900

    Bring-up the yoonsample.testcomm

--- yoonsample.testcomm/src/main/java/com/yoonsample/testcomm/Main.java
+++ yoonsample.testcomm/src/main/java/com/yoonsample/testcomm/Main.java
@@ -1,7 +1,48 @@
 package com.yoonsample.testcomm;
 
+import com.yoonfactory.comm.IYoonComm;
+import com.yoonfactory.comm.YoonServer;
+import com.yoonfactory.log.YoonConsoler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String args[]) {
-        System.out.println("Hello!");
+    public static YoonConsoler pCLM = new YoonConsoler();
+    public static IYoonComm pComm;
+    public static void main(String args[]) throws IOException {
+        System.out.println("Typing the module (TCPServer, TCPClient, Serial) >> ");
+        BufferedReader pReader = new BufferedReader(new InputStreamReader(System.in));
+        String strSelectedModule = pReader.readLine();
+        switch(strSelectedModule.toLowerCase()) {
+            case "tcpserver":
+                pCLM.write("Start TCPServer Module");
+                ProcessServer();
+                break;
+            case "tcpclient":
+                pCLM.write("Start TCPClient Module");
+                ProcessClient();
+                break;
+            case "serial":
+                pCLM.write("Start Serial Module");
+                ProcessSerial();
+                break;
+            default:
+                pCLM.write("Interrupt Module");
+                break;
+        }
+    }
+
+    public static void ProcessServer() {
+        //
+    }
+
+    public static void ProcessClient() {
+        //
+    }
+
+    public static void ProcessSerial() {
+        //
     }
 }
